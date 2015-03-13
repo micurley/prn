b05afedc3ecd5f96fd11dfba151f91ac5f109518
Morgan Curley <morgan.curley@gmail.com>
2015-03-12 20:49:20 -0400

    Rebuilt category pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
         $('.date-tear-sheet').hover(function() {
-             $(this).find('.date-tear-sheet-title').toggle(600);
+             $(this).find('.date-tear-sheet-title').fadeToggle(600);
        });
 });
